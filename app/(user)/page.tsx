84c241d50826985c4e65fd41167247569e5ed12b
faheem221 <faheemk793@gmail.com>
2025-06-17 19:14:47 +0530

    fixed

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./(homepage)/HeroSection";
 import About from "./(homepage)/About";
 import Choose from "./(homepage)/Choose";
 import HomeForm from "./(homepage)/HomeForm";
-import { ChartPieDonutText } from "./(homepage)/PieChart";
+
 import ServiceRevamp from "./(homepage)/serviceRevamp";
 import Blogs from "./(homepage)/Blogs";
 import Sector from "./(homepage)/Sector";
@@ -13,7 +13,7 @@ export default function Home() {
       <About />
       <ServiceRevamp />
       <Choose />
-      <ChartPieDonutText />
+
       <Sector />
       <Blogs />
       <HomeForm />
