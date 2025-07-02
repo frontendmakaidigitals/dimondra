7c8dfa94101d6334d49d897c908d53c27009214a
faheem221 <faheemk793@gmail.com>
2025-07-02 12:37:48 +0530

    button working

--- app/(user)/Services/(business-service-section)/CTA.tsx
+++ app/(user)/Services/(business-service-section)/CTA.tsx
@@ -94,7 +94,7 @@ const CTA = ({
             {desc}
           </p>
           <div className="flex justify-center mt-5 ">
-            <button className="px-4 bg-dimondra-black py-2 text-dimondra-white rounded-lg text-sm">
+            <button onClick={()=>setIsOpen(true)} className="px-4 bg-dimondra-black py-2 text-dimondra-white rounded-lg text-sm">
               {button}
             </button>
           </div>
