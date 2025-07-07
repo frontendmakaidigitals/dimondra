1cee6c3dedc141afb2f081b11d41a0c5111dd202
faheem221 <faheemk793@gmail.com>
2025-07-07 12:24:15 +0530

    fixed

--- app/(user)/Services/(business-service-section)/service.tsx
+++ app/(user)/Services/(business-service-section)/service.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { motion } from "motion/react";
 interface data {
   title: string;
   description: string;
@@ -14,28 +16,39 @@ const Service = ({
   title?: string;
 }) => {
   return (
-    <div className="container my-20">
-      <h1 className="text-4xl lg:text-5xl font-[600] text-dimondra-black tracking-tight max-w-3xl mx-auto text-center">
-        {title}
-      </h1>
-      {desc ? (
-        <p className="text-center mx-auto mt-5 max-w-2xl">{desc}</p>
-      ) : null}
-      <div className="grid  grid-cols-1 gap-0 lg:grid-cols-2">
-        {data.map(({ title, description, icon: Icon }, idx) => (
-          <div
-            className={`py-11 grid grid-cols-1 lg:grid-cols-2 lg:py-0 lg:border-b-0 ${idx === data.length - 1 ? "" : " border-b border-teal-900/50"}`}
-            key={idx}
-          >
-            <div className=" bg-green-100 rounded-lg p-2 w-fit">
-              <Icon className="size-[32px] stroke-dimondra-teal" />
-            </div>
-            <h2 className="text-3xl font-dmSans font-[500]">{title}</h2>
-            <p className="mt-4">{description}</p>
-          </div>
-        ))}
+    <section className="py-20 bg-[#D9E2C9]">
+      {" "}
+      <div className="container ">
+        <h1 className="text-4xl lg:text-5xl font-[600] text-[#012F13] tracking-tight max-w-3xl mx-auto text-center">
+          {title}
+        </h1>
+        {desc ? (
+          <p className="text-center mx-auto mt-5 max-w-2xl text-[#012F13]">
+            {desc}
+          </p>
+        ) : null}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-14">
+          {data.map(({ title, description, icon: Icon }, idx) => (
+            <motion.div
+              key={idx}
+              whileHover={{ y: -8, scale: 1.02 }}
+              transition={{ type: "spring", stiffness: 200, damping: 15 }}
+              className=" backdrop-blur-lg bg-[#C2D8AD] group hover:text-[#D9E2C9] hover:bg-[#012F13] rounded-2xl p-6 shadow-xl border-green-700/5 border-2  hover:shadow-2xl transition-all"
+            >
+              <div className="rounded-full bg-dimondra-100 text-dimondra-800  mb-6">
+                <Icon className="w-14 h-14 stroke-[#012F13] group-hover:stroke-[#D9E2C9]" />
+              </div>
+              <h2 className="text-3xl font-quicksand font-semibold group-hover:text-[#D9E2C9] text-[#012F13]">
+                {title}
+              </h2>
+              <p className="text-[#012F13]/80 group-hover:text-[#D9E2C9] font-dmSans mt-3">
+                {description}
+              </p>
+            </motion.div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
