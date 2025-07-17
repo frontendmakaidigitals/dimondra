f2b1c158e0e0a3f0b1152acc9c02ac1464b4a9f8
faheem221 <faheemk793@gmail.com>
2025-07-17 12:20:57 +0530

    chnaged firebase config

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,19 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAZeDuHjvZ94F4489trh86_HUQR-4CWDJ4",
-  authDomain: "expreiment-284ae.firebaseapp.com",
-  projectId: "expreiment-284ae",
-  storageBucket: "expreiment-284ae.firebasestorage.app",
-  messagingSenderId: "406779587772",
-  appId: "1:406779587772:web:371191ac36b8653bdbf4b9",
-  measurementId: "G-NJM2QPVWQ4",
+  apiKey: "AIzaSyCrb-cztRvxg3x7ZxHS-nSBbrx9ea1Hb4g",
+  authDomain: "dimondra-backend.firebaseapp.com",
+  projectId: "dimondra-backend",
+  storageBucket: "dimondra-backend.firebasestorage.app",
+  messagingSenderId: "559178153167",
+  appId: "1:559178153167:web:5d72ef4471e085268e8bba",
+  measurementId: "G-T19Q1TEGWS",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-
