eee25e53e10b1fc37bbb9fd2e7d5502647aa3ee7
faheem221 <faheemk793@gmail.com>
2025-08-14 18:26:12 +0530

    fixed

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -22,8 +22,8 @@ export async function POST(request: NextRequest) {
           quantity: 1,
         },
       ],
-      success_url: "http://localhost:3000/Services/HR-Services/Virtual-Admin-Support",
-      cancel_url: "http://localhost:3000/Services/HR-Services/Virtual-Admin-Support",
+      success_url: "https://Dimondra.com/Services/HR-Services/Virtual-Admin-Support",
+      cancel_url: "https://Dimondra.com/Services/HR-Services/Virtual-Admin-Support",
 
       customer_email: email,
 
