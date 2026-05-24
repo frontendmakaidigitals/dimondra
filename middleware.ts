e71183ce98ebdb4188d7bb3bd50b0c1f3f575156
faheem221 <faheemk793@gmail.com>
2026-05-25 02:34:15 +0530

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
+const baseUrl = 'http://dimondra.com/'
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("admin-auth")?.value;
   const { pathname } = request.nextUrl;
 
   if (!token) {
-    const loginUrl = new URL("/login", request.url);
-    loginUrl.searchParams.set("next", pathname);
-    return NextResponse.redirect(loginUrl);
+    const loginUrl = new URL("/login", baseUrl);
+loginUrl.searchParams.set("next", pathname);
+return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
